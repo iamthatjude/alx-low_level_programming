6ae73b0e9a2ca21e3a8155be9abef0949ae6ae57
Jude Vagrant ALX <counselme4@gmail.com>
2023-03-17 13:17:26 +0000

    Added: C program that prints out a given string using puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char string[] = "Programming is like building a multilingual puzzle";
+
+	puts(string);
+	return (0);
+}
