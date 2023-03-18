d3a6c9fca694ba48e403fa405c4fbf631d30442e
Jude Vagrant ALX <counselme4@gmail.com>
2023-03-18 14:11:47 +0000

    Added: Program to assign random number and print if positive, negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,18 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	while (n != 0)
+	if (n > 0)
 	{
-		if (n > 0)
-		{
-			printf("%d is postive\n", n);
-		}
-		else if (n <= 0)
-		{
-			printf("%d is negative\n", n);
-		}
+		printf("%d is postive\n", n);
 	}
-	printf("%d is zero\n", n);
+	else if (n <= 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
 	return (0);
 }
