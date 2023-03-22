26b81f8130fc2423d556ae323d8ee000124c2761
Jude Vagrant ALX <counselme4@gmail.com>
2023-03-22 07:57:59 +0000

    Added: Print alphabets in lowercase; use print_alphabet() and _putchar()

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,10 @@
 #include "main.h"
 
 /**
-* main - Prints alphabets in lowercase
+* print_alphabet - Prints alphabets in lowercase
 *
 * Return: Always 0
 */
-int main(void)
-{
-	print_alphabet();
-	_putchar('\n');
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	char start = 'a';
