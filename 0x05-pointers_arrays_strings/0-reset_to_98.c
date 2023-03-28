e269f2f284eae183491171f196715f651f45371d
Jude Vagrant ALX <counselme4@gmail.com>
2023-03-28 00:38:48 +0000

    Added: Change the value of a parameter using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Change the value of parameter
+*
+* @n: parameter whose value will be updated
+*
+* Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
