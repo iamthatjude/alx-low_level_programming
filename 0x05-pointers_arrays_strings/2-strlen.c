f6cf9e512f4fb6c71b7b3d3d52a81860a08dbffc
Jude Vagrant ALX <counselme4@gmail.com>
2023-03-28 01:11:16 +0000

    Added: Return the length of given string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strlen - Return the length of a string
+*
+* @s: the given string/character
+*
+* Return: Always 0.
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
