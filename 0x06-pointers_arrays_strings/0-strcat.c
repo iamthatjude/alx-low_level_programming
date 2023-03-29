1efd353b328f5a591d1c874b368fa989cfdc0b2c
Jude Vagrant ALX <counselme4@gmail.com>
2023-03-29 23:07:08 +0000

    Added: Concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_strcat - Concatenates two strings
+ *
+ * @dest: receiver string
+ * @src: string to be concatenated with @dest
+ *
+ * Return: Always 0
+*/
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
