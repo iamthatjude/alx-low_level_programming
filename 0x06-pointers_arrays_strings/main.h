d4a5943fb21ad3e5d79ab1857884dfcafa1baf61
Jude Vagrant ALX <counselme4@gmail.com>
2023-03-29 23:10:37 +0000

    Added: header file that carries function declarations for this project

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_EXT_
-#define _HEADER_EXT_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 /* Function Declaration: function type, names and parameters can come here */
 char *_strcat(char *dest, char *src);
