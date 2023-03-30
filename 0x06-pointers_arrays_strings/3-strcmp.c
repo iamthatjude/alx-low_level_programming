6540260ab9b4fb51729ffcf4bc57d2a4f46d5466
Jude Vagrant ALX <counselme4@gmail.com>
2023-03-30 13:52:09 +0000

    Added: Compare two strings with _strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare two strings
+ *
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: Always 0
+*/
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
