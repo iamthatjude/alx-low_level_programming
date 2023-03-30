3bc1a59aee2245e5217ba50a3769850a37896f20
Jude Vagrant ALX <counselme4@gmail.com>
2023-03-30 13:45:19 +0000

    Added: Copy strings with _strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,5 +11,16 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	return (strncpy(dest, src, n));
+	int count;
+
+	for (count = 0; count < n && src[count] != '\0'; count++)
+	{
+		dest[count] = src[count];
+	}
+	for ( ; count < n; count++)
+	{
+		dest[count] = '\0';
+	}
+
+	return (dest);
 }
