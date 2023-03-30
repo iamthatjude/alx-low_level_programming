38173279e7c433f1902087aa851f586c6502a2c6
Jude Vagrant ALX <counselme4@gmail.com>
2023-03-30 14:41:36 +0000

    Added: Compare two strings with _strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,9 +10,9 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int flag = 0;
+	int flag = 0, i;
 
-	for (int i = 0; i < (s1[i] != '\0') && (s2[i] != '\0'); i++)
+	for (i = 0; i < (s1[i] != '\0') && (s2[i] != '\0'); i++)
 	{
 		flag = s1[i] - s2[i];
 	}
