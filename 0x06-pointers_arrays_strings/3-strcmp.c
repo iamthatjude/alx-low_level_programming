066c58aafef08b9d1d08db6186846e8a6f480568
Jude Vagrant ALX <counselme4@gmail.com>
2023-03-30 14:23:17 +0000

    Added: Compare two strings with _strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,5 +10,10 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	return (strcmp(s1, s2));
+	if (s1 == s1)
+		return (strcmp(s1, s2));
+	else if (s1 < s2)
+		return (strcmp(s1, s2));
+	else if (s1 > s2)
+		return (strcmp(s1, s2));
 }
