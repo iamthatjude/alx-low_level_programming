e648e1565363c21423ab67eb32f80efffc9c4a85
Jude Vagrant ALX <counselme4@gmail.com>
2023-04-03 20:50:25 +0000

    Added: Copy memory area and return pointer to dest using _memcpy()

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copy memory area and return pointer to dest
+ *
+ * @dest: where content is copied to [destination]
+ * @src: where content is copied from [source]
+ * @n: number of bytes to copy
+ *
+ * Return: Always 0 | void
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int counter;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		dest[counter] = src[counter];
+	}
+
+	return (dest);
+}
