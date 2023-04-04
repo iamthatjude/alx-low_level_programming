3df547bff2a6cffde5ea676df9f81f38b50652d9
Jude Vagrant ALX <counselme4@gmail.com>
2023-04-04 00:11:51 +0000

    Added: Prints the chessboard using print_chessboard()

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints the chessboard
+ *
+ * @a: an array with a size of 8
+ *
+ * Return: void
+*/
+void print_chessboard(char (*a)[8])
+{
+    unsigned int i, j = 0;
+
+	while (i < 64)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			j = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - m]);
+		i++;
+	}
+	_putchar('\n');
+}
