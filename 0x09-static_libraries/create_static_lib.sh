9193d8e2f4401d5b98f632fc2e0ba8b74ed2e64f
Jude Vagrant ALX <counselme4@gmail.com>
2023-04-11 03:04:48 +0000

    Creating a Static Library using Bash

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #1. Use GCC to Compile the Object Code first
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
+gcc -Wall -pedantic -Werror -Wextra -c *.c
 
 #2. Archive or Create the Static Library file
 ar -rc liball.a *.o
