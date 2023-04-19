12f192d491a26c41b81e2ce4330329d825012d87
Jude Vagrant ALX <counselme4@gmail.com>
2023-04-19 06:08:39 +0000

    [Update] Removed unneeded space from dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@ typedef struct dog dog_t;
 
 /**
  * struct dog - struct that stores some information of a dog
- * 
+ *
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
