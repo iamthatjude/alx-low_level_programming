1f30b67552129b549120f2ef381a15288f2b5b6f
Jude Okpara <34810173+iamthatjude@users.noreply.github.com>
2023-04-19 05:03:45 +0000

    Project Files for 0x0D-preprocessor
    Contain all project files:
    - 0-object_like_macro.h
    - 1-pi.h
    - 2-main.c
    - 3-function_like_macro.h
    - 4-sum.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,7 @@
+#ifndef _PI_H_
+#define _PI_H_
+
+/* Macro Definition */
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,7 @@
+#ifndef _SUM_LIKE_MACRO_
+#define _SUM_LIKE_MACRO_
+
+/* Macro Definition */
+#define SUM(x, y) ((x) + (y))
+
+#endif
