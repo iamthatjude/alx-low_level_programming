326795c6fe64fc4c588fcd8dc648e99e66bc8bc6
Jude Vagrant ALX <counselme4@gmail.com>
2023-04-27 17:38:37 +0000

    [Update] "Hello, Holberton" in Assembly language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,18 @@
 section .text
-		global _start
+		global main
+		extern printf
 
-_start:
+main:
 		mov edx, len
 		mov ecx, msg
-		mov ebx, 1
-		mov eax, 4		;system call (sys_write)
-		int 0x80		;call kernel
+		;mov ebx, 1
+		;mov eax, 4		;system call (sys_write)
+		;int 0x80		;call kernel
+		call printf
+		ret
 
-		mov eax, 1		;system call (sys_exit)
-		int 0x080
+		;mov eax, 1		;system call (sys_exit)
+		;int 0x080
 
 section .data
 		msg db "Hello, Holberton", 0xa		;new line = 0xa or 10
