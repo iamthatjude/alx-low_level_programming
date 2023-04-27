580298db49b17edd6b2fab8d41e418f64bc35c64
Jude Vagrant ALX <counselme4@gmail.com>
2023-04-27 17:10:02 +0000

    [Update] Returns the number of elements in a linked list_t list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,3 +1,12 @@
+#include "lists.h"
+
+/**
+ * list_len - Returns then number of elements in a list
+ *
+ * @h: singly linked list
+ *
+ * Return: number of elements in the list
+ */
 size_t list_len(const list_t *h)
 {
 	size_t elements;
