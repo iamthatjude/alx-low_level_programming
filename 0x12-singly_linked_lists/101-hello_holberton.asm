548d44e6b2d13cca82a373b58ed999db802724ba
Jude Vagrant ALX <counselme4@gmail.com>
2023-04-27 18:13:51 +0000

    [Update] "Hello, Holberton" in Assembly language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,16 @@
 section .text
-		extern printf				;C function to be called
-		global main					;entry point
+		extern printf						;C function to be called
+		global main							;entry point
 
 main:
 		mov esi, msg
 		mov edi, fmt
 		mov eax, 0
-		call printf					;call C function => printf()
+		call printf							;call C function => printf()
 
-		mov eax, 0					;return value => 0
-		ret							;return
+		mov eax, 0							;return value => 0
+		ret									;return
 
 section .data
-msg: 	db "Hello, Holberton", 0	;terminate string with 0 in C
-fmt: 	db "%s", 10, 0				;printf format => '\n' or '0'
+		msg: 	db "Hello, Holberton", 0	;terminate string with 0 in C
+		fmt: 	db "%s", 10, 0				;printf format => '\n' or '0'
